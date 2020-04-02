d173cdadc9d62b0a0b5ec2481bc4a2a0fbead5f0
alexyuan00 <alexyuan00@gmail.com>
2020-04-02 18:01:09 -0500

    enter

--- Test.py
+++ Test.py
@@ -1,1 +1,11 @@
 print("This is for Github test")
+
+def type_of_album(artist, album, year_released):
+
+    print(artist, album, year_released)
+    if year_released > 2000:
+        return "Modern"
+    else:
+        return "Oldie"
+x= type_of_album("Taylor Swift", "Red", 2012)
+print(x)
