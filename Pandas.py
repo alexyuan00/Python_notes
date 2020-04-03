307f67606ec219853fb65c8d90a022f30082825f
alexyuan00 <alexyuan00@gmail.com>
2020-04-03 18:17:15 -0500

    test

---
+++ Pandas.py
@@ -0,0 +1,7 @@
+import pandas as pd
+pf = pd.DataFrame(
+    {'Countries' : ['USA', 'China', 'Canada'],
+     'Countries2' : ['USA', 'China', 'Canada']}
+
+)
+print(pf)
---
+++ numpy.py
@@ -0,0 +1,11 @@
+import numpy as np
+u = np.array([1, 0])
+v = np.array([0, 1])
+print(u-v)
+
+z = np.array([2, 4])
+print(-2*z)
+
+a = np.array([1, 2, 3, 4, 5])
+b = np.array([1, 0, 1, 0, 1])
+print(a * b)
